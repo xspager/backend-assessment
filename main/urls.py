936fc01b1226bb6db8a5a34ecca1f5b6de51c9a7
Daniel Lemos <xspager@gmail.com>
2020-12-22 16:50:21 -0300

    We need a way to list pending requests, no test for this tho

--- main/urls.py
+++ main/urls.py
@@ -14,6 +14,7 @@ from .views import IndexView
 urlpatterns = [
     path('product/activate', ActivateProductViewSet.as_view({'post': 'create'})),
     path(r'product/activate/<int:pk>', CancelProductActivationViewSet.as_view({'delete': 'destroy'})),
+    path(r'product/activation/', AdminProductActivationViewSet.as_view({'get': 'list'})),
     path(r'product/activation/approve/<int:pk>', AdminProductActivationViewSet.as_view({'post': 'approve'})),
     path(r'product/activation/reject/<int:pk>', AdminProductActivationViewSet.as_view({'post': 'reject'})),
     path('', IndexView.as_view())
